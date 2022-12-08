9d4b6e45fd331a57c85c09198f782eb98b940a7e
Mirko Franco <francomirko91@gmail.com>
2022-12-08 13:42:21 +0100

    -----------------

---
+++ src/Start.java
@@ -0,0 +1,15 @@
+public class Start {
+    public static void main(String[] args) {
+
+        String myName = "Mirko";
+
+        if (myName.length() % 2 == 0) {
+            System.out.println("Number of letters is even");
+        } else {
+            System.out.println("Number of letters is odd");
+
+        }
+
+
+    }
+}
